935562464d47341c7d28dfc7b3de4ebb668fb5a3
Shiaxu <shiaxu.inbox@gmail.com>
2025-04-16 17:24:34 +0300

    B: 60 fps Triangle appears

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
+from player import *
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.Surface.fill(screen, ("#000000"))
-    pygame.display.flip()
+
 
 def main():
     pygame.init
-    
+
+    clock = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.Surface.fill(screen, ("#000000"))
+        player1.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
