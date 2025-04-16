57a943d0e883382e8c5423fb3b7b87e2c05be556
Shiaxu <shiaxu.inbox@gmail.com>
2025-04-16 20:55:40 +0300

    C: IT WORKS AND IT LOOPS

--- circleshape.py
+++ circleshape.py
@@ -1,18 +1,37 @@
 import pygame
 from constants import *
 
+
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, PLAYER_RADIUS):
+
+    
+
+    def __init__(self, x, y, radius):
         # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
 
+
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
-        self.radius = PLAYER_RADIUS
+        self.radius = radius
+
+    def coordinate_reset(self, width, height):
+        x = width
+        y = height
+        if width >= SCREEN_WIDTH:
+            x = 0
+        if width <= 0:
+            x = SCREEN_WIDTH
+        if height >= SCREEN_HEIGHT:
+            y = 0
+        if height <= 0:
+            y = SCREEN_HEIGHT
+        self.position = pygame.Vector2(x, y)
+        
 
     def draw(self, screen):
         # sub-classes must override
@@ -21,3 +40,4 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
--- main.py
+++ main.py
@@ -7,21 +7,38 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 
+
 def main():
     pygame.init
 
     clock = pygame.time.Clock()
     dt = 0
-    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+
+
+    Player.containers = (updatable, drawable)   
+    
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                
         pygame.Surface.fill(screen, ("#000000"))
-        player1.draw(screen)
+        updatable.update(dt)
+        
+        for obj in drawable:
+            
+            obj.coordinate_reset(obj.position[0], obj.position[1])
+            obj.draw(screen)
+            print(f"PRINTED {obj.position}")
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
